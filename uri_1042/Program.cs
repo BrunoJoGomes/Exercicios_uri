eb9b4700d1fc12a8a973852b37627b385742b75d
BRUNO JOSE OLIVEIRA GOMES <bruno.jogomes@senacsp.edu.br>
2022-09-12 15:55:44 -0300

    Aula 12/09

--- uri_1042/Program.cs
+++ uri_1042/Program.cs
@@ -12,7 +12,63 @@ namespace uri_1042
             int n2 = int.Parse(numeros[1]);
             int n3 = int.Parse(numeros[2]);
 
-           
+            int maior = 0;
+            int menor = 0;
+            int meio = 0;
+            
+            if (n1 > n2 && n1 > n3)
+            {
+                maior = n1;
+                if (n2 < n3)
+                {
+                    menor = n2;
+                    meio = n3;
+                }
+                else
+                {
+                    menor = n3;
+                    meio = n2; 
+                }
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                maior = n2;
+                if (n1 < n3)
+                {
+                    menor = n1;
+                    meio = n3;
+                }
+                else
+                {
+                    menor = n3;
+                    meio = n1;
+                }
+            }
+            else if (n3 > n1 && n3 > n2)
+            {
+                maior = n3;
+                if (n1 < n2)
+                {
+                    menor = n1;
+                    meio = n2;
+                }
+                else
+                {
+                    menor = n2;
+                    meio = n1;
+                }
+            }
+            Console.WriteLine(menor);
+            Console.WriteLine(meio);
+            Console.WriteLine(maior);
+            Console.WriteLine();
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+
+
+
+
 
 
 
