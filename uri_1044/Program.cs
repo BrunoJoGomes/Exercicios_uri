27e0a3a04ad9aaf4bc9970aa7897b73622f84504
BRUNO JOSE OLIVEIRA GOMES <bruno.jogomes@senacsp.edu.br>
2022-09-13 14:19:24 -0300

    Aula 13/09

---
+++ uri_1044/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uri_1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numeros = Console.ReadLine().Split();
+
+            int n1 = int.Parse(numeros[0]);
+            int n2 = int.Parse(numeros[1]);
+
+            if (n2 % n1 == 0 || n1 % n2 == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
